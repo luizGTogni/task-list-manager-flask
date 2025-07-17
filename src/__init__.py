8758aa16ba969db871b759dc7437d1276c20aa1d
Luiz Togni <contato.togni@gmail.com>
2025-07-17 11:25:18 -0300

    feat: add flask sqlalchemy initial

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
 
 app = Flask(__name__)
-tasks = []
+app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASK_SQLALCHEMY_DB_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 from src import routes
