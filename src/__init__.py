6d8845cb44c1d09c04755a8c18b6ac2b08a265d1
Luiz Togni <contato.togni@gmail.com>
2025-07-17 13:59:05 -0300

    feat: add login manager

--- src/__init__.py
+++ src/__init__.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 load_dotenv()
 
@@ -10,6 +11,9 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("FLASK_SQLALCHEMY_DB_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
 db = SQLAlchemy(app)
 
